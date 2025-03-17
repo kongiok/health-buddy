5c23039a490177719c217925fa9e75da01d26c10
kongiok <me@ong.kongiok.tw>
2025-03-17 15:51:07 +0800

    refactor(interactive): Extend haptic feedback component
    Create reusable HapticPressable with configurable strength levels and
    cross-platform support; renamed component to make it more precisely.

---
+++ components/atoms/interactive/HapticPressable.tsx
@@ -0,0 +1,40 @@
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { HapticFeedbackStrength } from "@/constants/interactives";
+import { PlatformPressable } from "@react-navigation/elements";
+import { Platform } from "react-native";
+import * as Haptics from "expo-haptics";
+
+type FunctionProps = BottomTabBarButtonProps & {
+  HapticFeedbackStrength: HapticFeedbackStrength;
+};
+
+export function HapticPressable(props: FunctionProps) {
+  return (
+    <PlatformPressable
+      {...props}
+      onPressIn={(ev) => {
+        if (Platform.OS === "ios" || Platform.OS === "android") {
+          switch (props.HapticFeedbackStrength) {
+            case "heavy":
+              Haptics.impactAsync(
+                Haptics.ImpactFeedbackStyle.Heavy,
+              );
+              break;
+            case "medium":
+              Haptics.impactAsync(
+                Haptics.ImpactFeedbackStyle.Medium,
+              );
+              break;
+            case "light":
+            default:
+              Haptics.impactAsync(
+                Haptics.ImpactFeedbackStyle.Light,
+              );
+              break;
+          }
+        }
+        props.onPressIn?.(ev);
+      }}
+    />
+  );
+}
